6016315946b8c3c91d5710a357ea98e1048b4e4d
Zaverpon <Maxzaveriukha@gmail.com>
2025-10-30 10:49:47 +0100

    add msg for fitalik

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
@@ -3,7 +3,7 @@ package sk.uniba.fmph.dcs.terra_futura;
 public class SelectReward {
     public static void main(String[] args) {
         System.out.println("Everything working");
-        System.out.println("Vitalik ahoj kook");
+        System.out.println("Vitalik ahoj koo");
     }
 
 }
