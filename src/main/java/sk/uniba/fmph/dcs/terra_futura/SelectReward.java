4070cf7c0da036657a229db34709830cf1798a6f
Zaverpon <Maxzaveriukha@gmail.com>
2025-10-30 10:48:20 +0100

    Add SelectReward class basic structure

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/SelectReward.java
@@ -3,7 +3,7 @@ package sk.uniba.fmph.dcs.terra_futura;
 public class SelectReward {
     public static void main(String[] args) {
         System.out.println("Everything working");
-        System.out.println("Vitalik ahoj kok");
+        System.out.println("Vitalik ahoj kook");
     }
 
 }
